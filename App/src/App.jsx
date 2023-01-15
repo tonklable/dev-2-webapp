56e464cbea6073025bfa4809f35f246a275031f2
tetus6 <tetus.hk@gmail.com>
2023-01-15 14:14:19 +0900

    ui update: responsive navbar, card addition w clickable components

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import Navbar from './components/navbar'
+import Navbar from './components/navbar';
+import {CardSection} from './components/card';
 
 function App() {
   return (
     <>
       <Navbar/>
+      <CardSection/>
     </>
   );
 }
--- App/src/components/navbar.jsx
+++ App/src/components/navbar.jsx
@@ -9,23 +9,29 @@ const Navbar = () => {
                     <h1 class='ml-3 text-3xl text-white font-poppins font-bold'>TyTime</h1>
                 </div>            
             </div>
-            <div class="grid grid-cols-9 gap-2 mx-3">
-                <form class="relative flex flex-row flex-wrap items-center col-span-6 drop-shadow">
+            <div class="grid grid-cols-9 sm:gap-2 gap-x-2 gap-y-0 mx-3">
+                <form class="relative flex flex-row flex-wrap items-center col-span-9 sm:col-span-6 drop-shadow mt-2 sm:my-3">
                     <label for="search-box"></label>
                     <div class="absolute left-4">
                         <Search/>
                     </div>
-                    <input type="search" id="search-box" class="w-full p-2 pl-12 my-3 text-black border border-[#ff6565] rounded-full" placeholder="search for activities"/>
+                    <input type="search" id="search-box" class="w-full p-2 pl-14 text-black border border-[#ff6565] rounded-full" placeholder="search for activities"/>
                     <button type="submit" class="absolute text-black right-6">Search</button>
                 </form>
-                <div class="relative flex items-center justify-evenly col-span-2 border border-gray-600 rounded-full my-3 drop-shadow-sm">
-                    <div class="absolute left-3">
-                        <Filter/>
+                <div class="relative grid grid-flow-col grid-cols-5 col-span-6 sm:col-span-2 border border-gray-600 rounded-full my-3 drop-shadow-sm">
+                    <div class="flex items-center justify-center">
+                        <div class="absolute ml-1" onClick={() => alert('clicked!')}>
+                            <Filter/>
+                        </div>
                     </div>
-                    <button type="submit" class="text-black ml-10 p-2.5 px-1 pl-3 font-semibold text-sm border-l border-l-gray-600">Date</button>
-                    <button type="submit" class="text-black mr-2 p-2.5 px-1 pl-3 font-semibold text-sm border-l border-l-gray-600">Status</button>
+                    <div class="flex items-center justify-center col-start-2 col-end-4">
+                        <button type="submit" class="text-black w-full mx-1 p-2.5 px-1 font-semibold text-sm border-x border-x-gray-600">Date</button>
+                    </div>
+                    <div class="flex items-center justify-center col-span-2">
+                        <button type="submit" class="text-black w-full mx-1 p-2.5 px-1 pr-3 font-semibold text-sm">Status</button>
+                    </div>
                 </div>
-                <div class="flex items-center justify-center bg-[#ff6565] hover hover:bg-[#E44444] rounded-full my-3 drop-shadow">
+                <div class="flex items-center justify-center bg-[#ff6565] hover hover:bg-[#E44444] rounded-full my-3 drop-shadow col-span-3 sm:col-span-1">
                     <button type="submit" class="text-white font-semibold text-sm">Add Event</button>
                 </div>
             </div>
