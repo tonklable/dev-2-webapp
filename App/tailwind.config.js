2b17ca7e6702fa26ff3073db083f59c8933c3fec
tonklable <tonklalor2544@gmail.com>
2023-02-05 19:54:42 +0900

    Create Add Event Function
    Create Modal to add event from "Add Event" button

--- App/tailwind.config.js
+++ App/tailwind.config.js
@@ -2,12 +2,14 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js",],
   theme: {
     fontFamily: {
       'poppins': ['poppins']
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
