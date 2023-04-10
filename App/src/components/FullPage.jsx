257b8b1c905ceb502179b406e09d6d7744cccfea
tonklable <tonklalor2544@gmail.com>
2023-04-10 23:35:16 +0900

    Join-Unjoin Feature

--- App/src/components/FullPage.jsx
+++ App/src/components/FullPage.jsx
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Share, Location, User, UserGroup, Calendar, Clock } from '../assets/icons'
-
-
+import { doc, getDoc, updateDoc, collection, arrayUnion, arrayRemove } from "firebase/firestore";
+import { database } from "../Firebase";
 
 function FullPage(props) {
     const event = props.events.find(event => event.id === props.pageID)
-    console.log(props.userid)
-    console.log(event.ownerid)
+    const [username, setUsername] = useState("");
+    const usersRef = collection(database, 'users');
+    const userRef = props.userid ? doc(usersRef, props.userid) : null;
+
+
+    useEffect(() => {
+        if (userRef) {
+            getDoc(userRef).then((doc) => {
+                if (doc.exists()) {
+                    setUsername(doc.data().name);
+                }
+            }, (error) => {
+                console.error(error);
+            });
+        }
+    }, [usersRef, props.userid]);
+
+    const join = event.attendees.includes(username);
+
+    console.log(event.attendees.includes(username))
+    const joinSubmit = async (e) => {
+        e.preventDefault();
+        const eventRef = collection(database, "events");
+        const docRef = doc(eventRef, props.pageID);
+        if (props.userid) {
+            await updateDoc(docRef, {
+                attendees: arrayUnion(props.userid),
+            })
+            window.location.reload()
+        }
+        else {
+            props.openLogin(true)
+            props.closeFP(false)
+
+        }
+
+    }
+    const unJoinSubmit = async (e) => {
+        e.preventDefault();
+        const eventRef = collection(database, "events");
+        const docRef = doc(eventRef, props.pageID);
+        await updateDoc(docRef, {
+            attendees: arrayRemove(props.userid),
+        })
+        window.location.reload()
+    }
+
     return (
         <div>
             <div class="fixed top-0 left-0 right-0 bottom-0 z-40 bg-black opacity-75"></div>
@@ -36,11 +81,31 @@ function FullPage(props) {
                                     <label for="Note" class="block mt-2 mb-2 text-sm font-medium text-gray-900 dark:text-white">Note</label>
                                     <p class="w-full h-full flex p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1 whitespace-pre-wrap"> {event.note}</p>
                                 </div>
-                                <div class="">
-                                    <button class=" px-3 py-2 text-xl text-blue-100 bg-blue-600 rounded">Join</button>
-                                    {props.userid == event.ownerid && <button class=" px-3 py-2 text-xl text-blue-100 bg-red-600 rounded" onClick={() => { props.setDeleteEvent(true); props.closeFP(false) }}>Delete</button>}
+                                <div className="flex space-x-4">
+                                    {!join && <button
+                                        className="px-4 py-2 text-xl text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-md shadow-md transition-colors duration-200"
+                                        onClick={joinSubmit}
+                                    >
+                                        Join
+                                    </button>}
+                                    {join && <button
+                                        className="px-4 py-2 text-xl text-white bg-yellow-600 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 rounded-md shadow-md transition-colors duration-200"
+                                        onClick={unJoinSubmit}
+                                    >
+                                        Unjoin
+                                    </button>}
+                                    {props.userid === event.ownerid && (
+                                        <button
+                                            className="px-4 py-2 text-xl text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 rounded-md shadow-md transition-colors duration-200"
+                                            onClick={() => {
+                                                props.setDeleteEvent(true);
+                                                props.closeFP(false);
+                                            }}
+                                        >
+                                            Delete
+                                        </button>
+                                    )}
                                 </div>
-
 
                             </div>
 
